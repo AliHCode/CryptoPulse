eff9d39dfffd4d369b4a5a63e0afc436d0976dd3
Ali Hussnain <alihussnain3349@gmail.com>
2026-02-23 11:30:57 +0500

    fix: Recharts responsive container negative width warning

--- src/pages/CoinDetail.tsx
+++ src/pages/CoinDetail.tsx
@@ -224,7 +224,7 @@ export default function CoinDetail() {
                 </div>
               </div>
             ) : (
-              <ResponsiveContainer width="100%" height="100%">
+              <ResponsiveContainer width="100%" height="100%" minHeight={400}>
                 <LineChart data={history}>
                   <XAxis
                     dataKey="time"
